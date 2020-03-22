c8eaeb7c135b12bfcd8a18134e64bc03b331fd33
Terrence Bell <terrencekbell@gmail.com>
2020-03-22 04:42:59 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -10,13 +10,15 @@ class Person
     person.partner = self
   end
 
-end
 
+class PartnerError < StandardError
+end 
+end  
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-class PartnerError < StandardError
-end 
+
+ 
 
 
